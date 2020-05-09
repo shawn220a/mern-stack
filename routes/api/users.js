f1e0545f0b5bcfa53d7fbbd2dc8b27310c22d8d6
Shawn Tschoepe <shawn.42496@gmail.com>
2020-05-09 08:25:46 -0500

    Verify JWT of user

--- routes/api/users.js
+++ routes/api/users.js
@@ -72,7 +72,7 @@ router.post(
           res.json({ token });
         },
       );
-    } catch (error) {
+    } catch (err) {
       console.error(err.message);
       res.status(500).send('Server error');
     }
