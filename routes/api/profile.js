255c13d1dc88ffa9b4adedf70dc5d89243ddd5c1
Shawn Tschoepe <shawn.42496@gmail.com>
2020-05-09 21:04:54 -0500

    Add comments

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+// Routes prepended by api/profile
+
 router.get('/', (req, res) => res.send('Profile route'));
 
 module.exports = router;
